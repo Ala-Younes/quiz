050b6c380e33f6d773dfd27ac8f9bdba02446634
Ala Younes <ala.younes@atos.net>
2023-07-25 18:35:57 +0200

    feature: quiz score + ui score + reset

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,28 +9,51 @@ interface CardProps {
 
 function Card({ questions, initialValue = 0 }: CardProps) {
   const [questionIndex, setQuestionIndex] = useState(initialValue);
+  const [score, setScore] = useState(0);
 
-  const handleClick = () => {
-    const quizLength = questions.length - 1;
-    setQuestionIndex((currIndex) =>
-      currIndex < quizLength ? currIndex + 1 : initialValue
-    );
+  const isQuizFinished = () => {
+    return questionIndex === questions.length - 1;
   };
 
+  const handleClick = (isCorrect: boolean) => {
+    isCorrect && setScore((currScore) => currScore + 1);
+    setQuestionIndex((currIndex) =>
+      isQuizFinished() ? initialValue : currIndex + 1
+    );
+  };
+  const handleReset = () => {
+    setQuestionIndex(0);
+  };
   return (
     <div className="bg-primary-darkBlue rounded-2xl mx-auto p-8 shadow-black shadow-2xl w-[40rem]">
-      <div className="flex gap-8">
-        <div className="flex flex-col text-white gap-4 w-full">
-          <h1 className="text-4xl">
-            Question 1/<span className="text-2xl">4</span>
+      {isQuizFinished() ? (
+        <div className="flex flex-col justify-center items-center h-72 ">
+          <h1 className="text-white text-3xl">
+            You scored {score} out of {questions.length}
           </h1>
-          <h3 className="text-2xl">{questions[questionIndex]?.questionText}</h3>
+          <button
+            onClick={handleReset}
+            className="border outline-none border-violet-700 bg-blue-500 rounded-xl mt-16 py-4 px-4 hover:bg-accent-blueHover"
+          >
+            Reset
+          </button>
         </div>
-        <QuestionsList
-          onNextQuestion={handleClick}
-          question={questions[questionIndex]}
-        />
-      </div>
+      ) : (
+        <div className="flex gap-8">
+          <div className="flex flex-col text-white gap-4 w-full">
+            <h1 className="text-4xl">
+              Question {questionIndex + 1}/<span className="text-2xl">4</span>
+            </h1>
+            <h3 className="text-2xl">
+              {questions[questionIndex]?.questionText}
+            </h3>
+          </div>
+          <QuestionsList
+            onNextQuestion={handleClick}
+            question={questions[questionIndex]}
+          />
+        </div>
+      )}
     </div>
   );
 }
--- src/components/QuestionsList.tsx
+++ src/components/QuestionsList.tsx
@@ -3,20 +3,19 @@ import Button from "./Button";
 
 interface QuestionsListProps {
   question: Question;
-  onNextQuestion: (index: number) => void;
+  onNextQuestion: (isCorrect: boolean) => void;
 }
 
 function QuestionsList({ question, onNextQuestion }: QuestionsListProps) {
-
-  const handleClick = (index: number) => {
-    onNextQuestion(index);
+  const handleClick = (isCorrect: boolean) => {
+    onNextQuestion(isCorrect);
   };
-  
+
   return (
     <div className="flex flex-col justify-between gap-6 w-full">
-      {question.answerOptions.map((answerOption, index) => (
-        <Button onClick={() => handleClick(index)} key={index}>
-          {answerOption.answerText}
+      {question.answerOptions.map(({ answerText, isCorrect }, index) => (
+        <Button onClick={() => handleClick(isCorrect)} key={index}>
+          {answerText}
         </Button>
       ))}
     </div>
